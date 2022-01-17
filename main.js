90323d21d46d5de9f36fb03a37a6f5ca1438da70
parth-kardani <77710709+parth-kardani@users.noreply.github.com>
2022-01-17 13:12:18 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+
+}
+
+
+
+function setup(){ 
+ canvas=createCanvas(300,300);
+ canvas.center();
+ background("white");
+}
+
+
+
+function draw(){
+
+}
+
+
+function clear_canvas(){
+    background("white");
+}
